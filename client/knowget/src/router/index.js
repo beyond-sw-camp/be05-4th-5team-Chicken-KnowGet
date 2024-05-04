e0c4b19838929d8b275145a92841be4962c4fb2d
eujin99 <pinklusha999@gmail.com>
2024-05-04 16:06:20 +0900

    QnA 목록 게시글 추가(하드코딩), 페이징 처리 테스트 완료 (페이징 디자인 변경 필요)

--- client/knowget/src/router/index.js
+++ client/knowget/src/router/index.js
@@ -3,6 +3,7 @@ import UserLogin from '../views/UserLogin.vue'
 import SignUp from '../views/SignUp.vue'
 import QnAList from '../views/QnAList.vue'
 
+
 const routes = [
   { path: '/', component: UserLogin },
   { path: '/signup', component: SignUp },
