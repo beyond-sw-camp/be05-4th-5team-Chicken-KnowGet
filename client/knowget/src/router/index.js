2aff4819f7b915df3548fabffe3e973b871b36b4
eujin99 <pinklusha999@gmail.com>
2024-05-04 16:01:24 +0900

    QnA 목록보기 퍼블

--- client/knowget/src/router/index.js
+++ client/knowget/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import UserLogin from '../views/UserLogin.vue' 
 import SignUp from '../views/SignUp.vue'
+import QnAList from '../views/QnAList.vue'
 
 const routes = [
   { path: '/', component: UserLogin },
   { path: '/signup', component: SignUp },
+  { path: '/qna', component: QnAList },
 ]
 
 const router = createRouter({
---
+++ client/knowget/src/views/QnAList.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="qna-list">
+    <h1>너겟에게 물어봐</h1>
+    <input type="text" v-model="search" placeholder="질문 찾기">
+    <div class="post-container" v-for="post in filteredPosts" :key="post.id">
+      <div class="post-left">
+        <h3 @click="navigateToPost(post.id)" class="post-title">{{ post.title }}</h3>
+        <p>작성자 : {{ post.author }}</p>
+      </div>
+      <div class="post-right">
+        <p>{{ post.date }}</p>
+        <p>조회 수: {{ post.views }}</p>
+        <p>답변 수: {{ post.comments.length }}</p>
+      </div>
+    </div>
+    <div class="pagination">
+      <button @click="setPage(p)" v-for="p in totalPages" :key="p" class="page-button">{{ p }}</button>
+    </div>
+    <button @click="$router.push('/write')">질문하러 가기</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: '',
+      posts: [
+        //포스팅 내용 넣어주세용
+      ],
+      currentPage: 1,
+      postsPerPage: 10
+    }
+  },
+  computed: {
+    filteredPosts() {
+      let start = (this.currentPage - 1) * this.postsPerPage;
+      let end = start + this.postsPerPage;
+      return this.posts.filter(post => post.title.includes(this.search)).slice(start, end);
+    },
+    totalPages() {
+      return Math.ceil(this.posts.filter(post => post.title.includes(this.search)).length / this.postsPerPage);
+    }
+  },
+  methods: {
+    setPage(page) {
+      this.currentPage = page;
+    },
+    navigateToPost(postId) {
+      this.$router.push(`/qna/${postId}`);
+    }
+  }
+}
+</script>
+
+<style>
+.qna-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+.post-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  border-bottom: 1px solid #ccc;
+  padding: 10px;
+  margin: 5px;
+}
+.post-left {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.post-right {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+.post-title {
+  cursor: pointer;
+  color: blue;
+}
+.pagination {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+.page-button {
+  border: none;
+  background-color: white;
+  color: black;
+  cursor: pointer;
+  padding: 5px 10px;
+  margin: 0 5px;
+  border-radius: 5px;
+  transition: background-color 0.3s, color 0.3s;
+}
+.page-button:hover {
+  background-color: #000000;
+  color: white;
+}
+button {
+  margin-top: 5px;
+}
+</style>
